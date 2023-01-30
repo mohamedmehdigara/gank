089a89201a46340ed63e51a6c3bf4d643736957d
mohamedmehdigara <garamehdi57@yahoo.com>
2023-01-30 15:23:29 +0100

    added files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-const withOffline = require('next-offline')
+ [{
+    "instancePath": "",
+    "schemaPath": "#/additionalProperties",
+    "keyword": "additionalProperties",
+    "params": {
+      "additionalProperty": "optimizedImagesInDev"
+    },
+    "message": "must NOT have additional properties"
+  }]
+
+  const withOffline = require('next-offline')
 const withImages = require('next-images')
+const path = require('path');
 
 const nextConfig = {
-  cssModules: true
+   sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = withOffline(withImages(nextConfig))
